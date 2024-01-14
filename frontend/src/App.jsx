32b6539fa1cd24be9b6a278d4eacaf6d1389668e
shwetd19 <shwetas.dhake@mitaoe.ac.in>
2024-01-14 10:57:22 +0530

    Header is completed with routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,17 +3,23 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
-import Profile from "./pages/Profile";  
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
+// import PrivateRoute from "./components/PrivateRoute";
 
 export default function App() {
   return (
     <BrowserRouter>
+      {/* header */}
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/profile" element={<Profile />} />
+        {/* <Route element={<PrivateRoute />}> */}
+          <Route path="/profile" element={<Profile />} />
+        {/* </Route> */}
       </Routes>
     </BrowserRouter>
   );
