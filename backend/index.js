5c09299894c80d8501e48cc4d759f9e872bd8bd3
shwetd19 <shwetas.dhake@mitaoe.ac.in>
2024-01-17 15:59:55 +0530

    production ready

--- backend/index.js
+++ backend/index.js
@@ -24,9 +24,6 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
-// Serve static files from the 'dist' directory
-// app.use(express.static(path.join(__dirname, 'client', 'dist')));
-
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
